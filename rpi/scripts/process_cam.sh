f33ee9fc3b195cb9b9a74e4333e2dfa9399b3300
alexander.ershov <alexander.ershov@gmail.com>
2017-09-29 21:19:11 +0300

    rpi camera

--- rpi/scripts/process_cam.sh
+++ rpi/scripts/process_cam.sh
@@ -10,10 +10,12 @@ if [ -e $cfile ] ; then
     cnt=$(cat $cfile)
 fi
 
-wget http://localhost:8080/?action=snapshot -O snapshot.jpg
+#wget http://localhost:8080/?action=snapshot -O snapshot.jpg
 
-timestamp=`stat -c %y snapshot.jpg`
-convert snapshot.jpg -fill white -pointsize 24 -draw "text 15,20 '${timestamp:0:19}'" /mnt/dav/outcam.tmp
+#timestamp=`stat -c %y snapshot.jpg`
+#convert snapshot.jpg -fill white -pointsize 24 -draw "text 15,20 '${timestamp:0:19}'" /mnt/dav/outcam.tmp
+
+raspistill -n -hf -vf -ae 128 -a 12 -o snapshot.jpg
 
 #every 30th run
 if ! ((cnt % 30)); then
@@ -28,7 +30,9 @@ fi
 #every one run
 cp /mnt/dav/outcam.jpg /mnt/dav/outcam_1.jpg
 
-cp /mnt/dav/outcam.tmp /mnt/dav/outcam.jpg
+#cp /mnt/dav/outcam.tmp /mnt/dav/outcam.jpg
+
+cp snapshot.jpg /mnt/dav/outcam.jpg
 
 # counter increase
 
